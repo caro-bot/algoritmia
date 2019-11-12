9c1779599d331bcc4d74e34133f94973c313ad39
Carolina Corcino <57647284+caro-bot@users.noreply.github.com>
2019-11-11 19:52:33 -0600

    Update homunculo.c

--- homunculo.c
+++ homunculo.c
@@ -1,25 +1,20 @@
 #include <stdlib.h> 
 #include <stdio.h>
 #include <string.h> 
-
 int main() {
     int n = 0;
     int m = 0;
     char* currentM;
     char* secuenciaN = NULL;
 
-    printf("Ingresa n: \n");
     scanf("%d", &n);
 
     secuenciaN = (char*)malloc(n + 1);
-
-    printf("Ingresa la secuencia de materiales presentes: \n");
+    for(int i=1;i<=n;i++){
     scanf("%s", secuenciaN);
-
-    printf("Ingresa el total materiales requeridos: \n");
+ }
     scanf("%d", &m);
 
-    printf("A continuacion, ingresaras la secuencia genetica de matertiales:\n");
     for( int alfa = 0; alfa < m; alfa ++ ){
         scanf("%s", currentM);
         if (strstr(secuenciaN, currentM) != NULL) {
